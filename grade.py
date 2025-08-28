8e66fd38c32b189fb8c21e6f8810bc7edb7e4c1c
nanaequeyaasparkles-sketch <nanaequeyaasparkles@gmail.com>
2025-08-28 01:36:09 +0100

    Update grade.py

--- grade.py
+++ grade.py
@@ -3,7 +3,7 @@ st.title("Grade calculator")
 name=st.text_input("Enter your name")
 index=st.text_input("Enter your index number")
 Course_name=st.selectbox("Enter Course name:",["Biology","Chmistry","Maths","English","Physics",])
-course_mark=st.number_input("Enter your course mark")
+course_mark=st.text_input("Enter your course mark")
 if st. button("Calculate grade"):
 	
 	course_mark= int(course_mark)/int(140)*100
@@ -19,4 +19,5 @@ if st. button("Calculate grade"):
 	   st. success("Fail")
 course_name=course_mark
 print(course_mark)
+
 
